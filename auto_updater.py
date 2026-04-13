f1660df3baef07baac50da7a49477d6e435851ff
mlk0622 <Bouaissimalik@gmail.com>
2026-04-13 09:00:24 +0200

    v2.7.7

--- auto_updater.py
+++ auto_updater.py
@@ -25,7 +25,7 @@ from urllib.error import URLError, HTTPError
 UPDATE_SERVER_URL = "https://api.github.com/repos/mlk0622/BayBay/releases/latest"
 
 # Version actuelle de l'application (fallback si BAYBAY_VERSION n'est pas defini)
-CURRENT_VERSION = "2.7.6"
+CURRENT_VERSION = "2.7.7"
 # Fichier de configuration local
 CONFIG_FILE = "update_config.json"
 
